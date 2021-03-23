b302e84035328e2376072c19aefe789a63d4ca2c
cwoodrow <chris.woodrow@maif.fr>
2021-03-23 23:35:55 +0100

    Update mongo import

--- mongodb/import.js
+++ mongodb/import.js
@@ -1,36 +1,54 @@
-const {MongoClient} = require('mongodb');
+const {
+  MongoClient
+} = require('mongodb');
 const csv = require('csv-parser');
 const fs = require('fs');
-const { mainModule } = require('process');
+const {
+  mainModule
+} = require('process');
 
 const MONGO_URL = 'mongodb://localhost:27017/';
-const DB_NAME = '911-calls'
+const DB_NAME = '911-calls';
+const COLLECTION_NAME = 'calls';
 
-const insertCalls = async function(db, callback) {
-    const collection = db.collection('calls');
+const insertCalls = async function (db, callback) {
+  const collection = db.collection(COLLECTION_NAME);
+  await dropCollectionIfExists(db, collection);
 
-    const calls = [];
-    fs.createReadStream('../911.csv')
-        .pipe(csv())
-        .on('data', data => {
-            const call = {}; // TODO créer l'objet call à partir de la ligne
-            calls.push(call);
-        })
-        .on('end', () => {
-          collection.insertMany(calls, (err, result) => {
-            callback(result)
-          });
-        });
+  const calls = [];
+  fs.createReadStream('../911.csv')
+    .pipe(csv())
+    .on('data', data => {
+
+      const call = {}; // TODO créer l'objet call à partir de la ligne
+
+      calls.push(call);
+    })
+    .on('end', () => {
+      collection.insertMany(calls, (err, result) => {
+        callback(result)
+      });
+    });
 }
 
-MongoClient.connect(MONGO_URL, (err, client) => {
+MongoClient.connect(MONGO_URL, {
+  useUnifiedTopology: true
+}, (err, client) => {
   if (err) {
-      console.error(err);
-      throw err;
+    console.error(err);
+    throw err;
   }
   const db = client.db(DB_NAME);
   insertCalls(db, result => {
-      console.log(`${result.insertedCount} actors inserted`);
-      client.close();
+    console.log(`${result.insertedCount} calls inserted`);
+    client.close();
   });
 });
+
+async function dropCollectionIfExists(db, collection) {
+  if (await db.listCollections({
+    name: COLLECTION_NAME
+  }).toArray().length > 0) {
+    await collection.drop();
+  }
+}
